7e3f4988f425d60a978eda098f3dd17102c3115e
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2019-01-27 20:09:46 -0500

    Finish for video demo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, redirect
 
 app = Flask(__name__)
 
@@ -11,6 +11,9 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/submit-file', methods=['POST'])
+def submit_file():
+    return redirect('/progress')
 
 
 @app.route('/progress', methods=['GET'])
@@ -21,16 +24,12 @@ def progress():
     return render_template('progress.html')
 
 
-
-
 @app.route('/finish', methods=['GET'])
 def finish():
     """
     Gets the finish page
     """
     return render_template('finish.html')
-
-
 
 
 if __name__ == '__main__':

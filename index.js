ae91ea32e15d3484e1ed77189e50f1133d0a6945
Brian Glowniak <bglowniak@gmail.com>
2019-02-13 16:33:31 -0500

    Directory changes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const BrowserWindow = electron.BrowserWindow
 
 function createWindow() {
     let win = new BrowserWindow({ width: 800, height: 600 })
+
+    win.loadFile('./app/templates/base.html')
 }
 
 app.on('ready', createWindow)

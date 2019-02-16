ec603ffbf2fd79daba54f4eb35d877d35444d0f9
Brian Glowniak <bglowniak@gmail.com>
2019-02-15 22:52:25 -0500

    Finished up UI functionality (for the most part)

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,6 +1,6 @@
 from fbs_runtime.application_context import ApplicationContext
 from functools import partial
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QWidget, QFileDialog, QComboBox, QRadioButton, QDateTimeEdit, QStackedWidget, QProgressBar
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QWidget, QFileDialog, QComboBox, QRadioButton, QDateTimeEdit, QStackedWidget, QProgressBar, QMessageBox
 
 from PyQt5.QtCore import QDateTime
 
@@ -30,15 +30,15 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("CDC/ATSDR Air Quality Analysis v" + version)
         self.setFixedSize(500, 250)
 
-    def start_analysis(self, filename, averaging_duration):
-        print("Starting analysis with " + filename + " and " + averaging_duration)
+    def start_analysis(self, filename, averaging_duration, time_selected, start_time, end_time):
         self.master.setCurrentIndex(1)
-        self.progress_widget.begin_progress()
+        self.progress_widget.begin_progress(filename, averaging_duration, start_time, end_time)
 
     def complete_analysis(self):
         self.master.setCurrentIndex(2)
 
     def start_over(self):
+        self.master.widget(0).reset()
         self.master.setCurrentIndex(0)
 
 
@@ -66,12 +66,14 @@ class MainWidget(QWidget):
         layout = QHBoxLayout()
         layout.addWidget(QLabel("Select Data File:"))
 
-        file_name = QLabel("No File Selected")
+        self.file_name = QLabel("No File Selected")
         button = QPushButton("Browse")
-        button.clicked.connect(partial(self.get_file, file_name))
+        button.clicked.connect(partial(self.get_file, self.file_name))
 
-        layout.addWidget(file_name)
+        layout.addWidget(self.file_name)
         layout.addWidget(button)
+
+        self.file = None
 
         return layout
 
@@ -112,6 +114,8 @@ class MainWidget(QWidget):
         layout.addWidget(yes_rb)
         layout.addWidget(no_rb)
 
+        self.time_selected = False
+
         return layout
 
     def time_selectors(self):
@@ -119,17 +123,17 @@ class MainWidget(QWidget):
         layout = QHBoxLayout()
         layout.addWidget(QLabel("Start Time: "))
 
-        start = QDateTimeEdit(self)
-        start.setCalendarPopup(True)
-        start.setDateTime(QDateTime.currentDateTime())
-        layout.addWidget(start)
+        self.start = QDateTimeEdit(self)
+        self.start.setCalendarPopup(True)
+        self.start.setDateTime(QDateTime.currentDateTime())
+        layout.addWidget(self.start)
 
         layout.addWidget(QLabel("End Time: "))
 
-        end = QDateTimeEdit(self)
-        end.setCalendarPopup(True)
-        end.setDateTime(QDateTime.currentDateTime())
-        layout.addWidget(end)
+        self.end = QDateTimeEdit(self)
+        self.end.setCalendarPopup(True)
+        self.end.setDateTime(QDateTime.currentDateTime())
+        layout.addWidget(self.end)
 
         widget.setLayout(layout)
 
@@ -146,11 +150,41 @@ class MainWidget(QWidget):
     def rb_state(self, clicked):
         if clicked.text() == "Yes" and clicked.isChecked() == True:
             self.time_selectors.show()
+            self.time_selected = True
         elif clicked.text() == "No" and clicked.isChecked() == True:
             self.time_selectors.hide()
+            self.time_selected = False
 
     def begin_process(self):
-        self.parentWidget().parentWidget().start_analysis(self.file, self.averaging_duration)
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Warning)
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.setWindowTitle("Input Error")
+        if not self.file:
+            msg.setText("No File Selected")
+            msg.setInformativeText("Please Select a Valid File")
+            msg.exec_()
+            return
+
+        if self.time_selected:
+            start_time = self.start.dateTime()
+            end_time = self.end.dateTime()
+            if start_time > end_time:
+                msg.setText("Invalid Time Entered")
+                msg.setInformativeText("End Time cannot be greater than Start Time")
+                msg.exec_()
+                return
+        else:
+            start_time = None
+            end_time = None
+
+        self.parentWidget().parentWidget().start_analysis(self.file, self.averaging_duration, self.time_selected, start_time, end_time)
+
+    def reset(self):
+        self.file = None
+        self.file_name.setText("No File Selected")
+        self.start.setDateTime(QDateTime.currentDateTime())
+        self.end.setDateTime(QDateTime.currentDateTime())
 
 class ProgressWidget(QWidget):
     def __init__(self):
@@ -159,12 +193,31 @@ class ProgressWidget(QWidget):
         self.layout = QVBoxLayout()
         self.layout.addWidget(QLabel("Analysis in progress..."))
 
+        self.filename_label = QLabel("")
+        self.averaging_label = QLabel("")
+        self.start_label = QLabel("")
+        self.end_label = QLabel("")
+
+        self.layout.addWidget(self.filename_label)
+        self.layout.addWidget(self.averaging_label)
+        self.layout.addWidget(self.start_label)
+        self.layout.addWidget(self.end_label)
+
         self.progress = QProgressBar()
         self.layout.addWidget(self.progress)
 
         self.setLayout(self.layout)
 
-    def begin_progress(self):
+    def begin_progress(self, filename, averaging, start, end):
+        self.filename_label.setText("File Name: " + filename)
+        self.averaging_label.setText("Averaging Duration: " + averaging)
+        if start != None and end != None:
+            self.start_label.setText("Start Time: " + start.toString())
+            self.end_label.setText("End Time: " + end.toString())
+        else:
+            self.start_label.setText("Start Time: N/A")
+            self.end_label.setText("End Time: N/A")
+
         self.completed = 0
 
         while self.completed < 100:
@@ -181,7 +234,14 @@ class CompleteWidget(QWidget):
         self.layout = QVBoxLayout()
         self.layout.addWidget(QLabel("Analysis Complete!"))
 
+        self.start_over = QPushButton("Start Over")
+        self.start_over.clicked.connect(self.reset)
+        self.layout.addWidget(self.start_over)
+
         self.setLayout(self.layout)
+
+    def reset(self):
+        self.parentWidget().parentWidget().start_over()
 
 
 if __name__ == '__main__':

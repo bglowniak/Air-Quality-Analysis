552099410a6407190a1341561e0faa2baa1823e6
jkatz35 <k.joel969@gmail.com>
2019-03-29 17:07:39 -0400

    refactor clean.py

--- src/main/python/clean.py
+++ src/main/python/clean.py
@@ -1,8 +1,8 @@
 import os
 import dateutil.parser
 import pandas as pd
+import numpy as np
 from enum import Enum
-from abc import ABC, abstractmethod
 
 class Sensor(Enum):
     AIR_BEAM = 1
@@ -10,35 +10,51 @@ class Sensor(Enum):
     AIR_EGG = 3
     INVALID = 4
 
-def process_file(filepath, start_time=None, stop_time=None, averaging_range=None):
+def process_file(filepath, output_path=None, start_time=None, stop_time=None, averaging_range=None):
     '''Interface to Front End
 
     @param filepath: string path to file input
+    @param output_path: string path to output directory
     @param start_time: string or python time object for start of range to filter data
     @param stop_time: string or python time object for stop of range to filter data
     @param averaging_range: string or pythong time object for averaging range
 
     @result String filepath for resulting PDF file
     '''
-    file_dir = os.path.dirname(filepath)
-    #temp
-    cleaned_dir = os.path.join(file_dir, 'data_out')
-    if not os.path.exists(cleaned_dir):
-        os.makedirs(cleaned_dir)
-    #temp
-    data = read_file(filepath)
-    data_type = identify_file(data)
-    if data_type == Sensor.AIR_BEAM:
-        data_obj = Air_Beam(data)
-    elif data_type == Sensor.AIR_EGG:
-        data_obj = Air_Egg(data)
-    elif data_type == Sensor.PURPLE_AIR:
-        data_obj = Purple_Air(data)
-    data_obj.clean()
-    #data_obj.make_pdf()
-    #below is temporary, should be changed in the future
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    data_obj = Data_File(filepath, output_path)
     print(data_obj.data_frame.head())
-    return data_obj.write_csv(cleaned_dir, data_type)
+    #return the output file path
+    return
+
+class Data_File():
+    '''Base Data File Class
+
+    @param data_frame: pandas data frame object to set as instance data for this data file
+    @attribute sensor_type: Sensor enum value, calculated from identify_file() function
+    TODO: remove this? @function clean: must be overwritten in subclass to clean based on object type
+    TODO: @function make_pdf: 
+    '''
+
+    def __init__(self, filepath, output_path):
+        self.output_path = output_path
+        self.data_frame = read_file(filepath)
+        self.sensor_type = identify_file(self.data_frame)
+        self.clean()
+
+    def clean(self):
+        if self.sensor_type == Sensor.AIR_BEAM:
+            self.data_frame = clean_air_beam(self.data_frame)
+        elif self.sensor_type == Sensor.AIR_EGG:
+            self.data_frame = clean_air_egg(self.data_frame)
+        elif self.sensor_type == Sensor.PURPLE_AIR:
+            self.data_frame = clean_purple_air(self.data_frame)
+
+        self.data_frame['Datetime'] = self.data_frame['Datetime'].apply(parse_time_string)
+        self.data_frame = self.data_frame.sort_values(by = 'Datetime')
+
 
 def read_file(file_path):
     #reads file and returns pandas dataframe
@@ -62,65 +78,36 @@ def identify_file(data_frame):
         return Sensor.AIR_EGG
     return Sensor.INVALID
 
-class Data_File(ABC):
-    '''Abstract base class for a data file, subclasses must overwrite the clean function
+def clean_purple_air(data_frame):
+    data_frame.columns = ['Datetime', 'entry_id', 'PM1.0', 'PM2.5', 'PM10.0', 'UptimeMinutes', 'RSSI_dbm', 'Temperature', 'Humidity', 'Pm2.5_CF_1_ug/m3']
+    return data_frame
 
-    @param data_frame: pandas data frame object to set as instance data for this data file
+def clean_air_egg(data_frame):
+    data_frame.columns = ['Datetime', 'Temperature', 'Humidity', 'SO2[ppb]', 'SO2[V]', 'PM1.0', 'PM2.5', 'PM10.0', 'Pressure', 'Latitude', 'Longitude', 'Altitude']
+    return data_frame
 
-    @function clean: must be overwritten in subclass to clean based on object type
-    TODO: @function make_pdf: 
-    '''
-    def __init__(self, data_frame):
-        self.data_frame = data_frame
-
-    @abstractmethod
-    def clean(self):
-        pass
-
-    def write_csv(self, file_dir, data_type):
-        #probably should delete this method eventually
-        fn = ''
-        if data_type == Sensor.AIR_BEAM:
-            fn = 'Air_Beam_Output.csv'
-        elif data_type == Sensor.PURPLE_AIR:
-            fn = 'Purple_Air_Output.csv'
-        elif data_type == Sensor.AIR_EGG:
-            fn = 'Air_Egg_Output.csv'
-        self.data_frame.to_csv(os.path.join(file_dir, fn))
-
-class Purple_Air(Data_File):
-    def clean(self):
-        self.data_frame.columns = ['Datetime', 'entry_id', 'PM1.0', 'PM2.5', 'PM10.0', 'UptimeMinutes', 'RSSI_dbm', 'Temperature', 'Humidity', 'Pm2.5_CF_1_ug/m3']
-        self.data_frame['Datetime'] = self.data_frame['Datetime'].apply(parse_time_string)
-
-class Air_Egg(Data_File):
-    def clean(self):
-        self.data_frame.columns = ['Datetime', 'Temperature', 'Humidity', 'SO2[ppb]', 'SO2[V]', 'PM1.0', 'PM2.5', 'PM10.0', 'Pressure', 'Latitude', 'Longitude', 'Altitude']
-        self.data_frame['Datetime'] = self.data_frame['Datetime'].apply(parse_time_string)
-
-class Air_Beam(Data_File):
-    def clean(self):
-        #TODO: if the values can change on different runs of this sensor, this will need to be changed to be more dynamic
-        #That means using the splits, find the Value name, then add the dataframes to a list, then iterate over that list to merge
-        beam = self.data_frame
-        splits = list(beam[beam['sensor:model'] == 'sensor:model'].index)
-        df1 = beam.iloc[0:splits[0]]
-        df2 = beam.iloc[splits[0]:splits[1]]
-        df3 = beam.iloc[splits[1]:splits[2]]
-        df4 = beam.iloc[splits[2]:splits[3]]
-        df1.columns = ['Datetime', 'Latitude', 'Longitude', 'Temperature']
-        df1 = df1.drop([0,1])
-        df2.columns = ['Datetime', 'Latitude', 'Longitude', 'Humidity']
-        df2 = df2.drop(df2.index[0:3])
-        df3.columns = ['Datetime', 'Latitude', 'Longitude', 'PM2.5']
-        df3 = df3.drop(df3.index[0:3])
-        df4.columns = ['Datetime', 'Latitude', 'Longitude', 'PM10.0']
-        df4 = df4.drop(df4.index[0:3])
-        beam = pd.merge(df1, df2, how='outer', on=['Datetime', 'Latitude', 'Longitude'])
-        beam = pd.merge(beam, df3, how='outer', on=['Datetime', 'Latitude', 'Longitude'])
-        beam = pd.merge(beam, df4, how='outer', on=['Datetime', 'Latitude', 'Longitude'])
-        beam['Datetime'] = beam['Datetime'].apply(parse_time_string)
-        self.data_frame = beam
+def clean_air_beam(data_frame):
+    #TODO: if the values can change on different runs of this sensor, this will need to be changed to be more dynamic
+    #That means using the splits, find the Value name, then add the dataframes to a list, then iterate over that list to merge
+    beam = data_frame
+    splits = list(beam[beam['sensor:model'] == 'sensor:model'].index)
+    df1 = beam.iloc[0:splits[0]]
+    df2 = beam.iloc[splits[0]:splits[1]]
+    df3 = beam.iloc[splits[1]:splits[2]]
+    df4 = beam.iloc[splits[2]:splits[3]]
+    df1.columns = ['Datetime', 'Latitude', 'Longitude', 'Temperature']
+    df1 = df1.drop([0,1])
+    df2.columns = ['Datetime', 'Latitude', 'Longitude', 'Humidity']
+    df2 = df2.drop(df2.index[0:3])
+    df3.columns = ['Datetime', 'Latitude', 'Longitude', 'PM2.5']
+    df3 = df3.drop(df3.index[0:3])
+    df4.columns = ['Datetime', 'Latitude', 'Longitude', 'PM10.0']
+    df4 = df4.drop(df4.index[0:3])
+    beam = pd.merge(df1, df2, how='outer', on=['Datetime', 'Latitude', 'Longitude'])
+    beam = pd.merge(beam, df3, how='outer', on=['Datetime', 'Latitude', 'Longitude'])
+    beam = pd.merge(beam, df4, how='outer', on=['Datetime', 'Latitude', 'Longitude'])
+    data_frame = beam
+    return data_frame
 
 def parse_time_string(s):
     d = dateutil.parser.parse(s)
@@ -135,4 +122,4 @@ if __name__ == "__main__":
 
     for i in ['Air_beam_7_31_8.22', 'air_egg', 'Purple_air']:
         filename = os.path.join(app_folder, r'data/' + i + '.csv')
-        process_file(filename)
+        process_file(filename, r'C:\Users\Joel\Documents\GT Courses\Junior Design\Air-Quality-Analysis\data\data_out')

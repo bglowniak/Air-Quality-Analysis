490e6888e56e6721790903545a5ff32f39b034e4
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2019-04-04 20:25:16 -0400

    Add script to generate sample PDF

---
+++ src/main/python/generate_pdf.py
@@ -0,0 +1,38 @@
+from fpdf import FPDF
+import os
+
+
+class ReportPDF(FPDF):
+    def header(self):
+        self.set_font('Times', 'B', 15)  # Times New Roman, Bold, 15
+
+        # Move to the right, and print title
+        self.cell(w=70)
+        self.cell(w=60, h=10, txt='Example Title', align='C', border=1, ln=0, link='C')
+
+        # Line break
+        self.ln(5)
+
+    # Page footer
+    def footer(self):
+        self.set_font('Times', '', 10)
+
+        self.set_xy(-60, -20)
+        self.cell(w=50, h=10, txt='Sample Footer Text')
+
+
+if __name__ == '__main__':
+    # Setup PDF
+    pdf = ReportPDF()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font('Times', '', 11)
+
+    pdf.cell(w=0, h=10, txt='Hello', border=0, ln=0)
+
+    # Write to output file
+    output_filename = os.path.join('', 'output.pdf')
+    pdf.output(output_filename, dest='F')
+
+    # Uncomment below to open the file (only works on macOS)
+    os.system('open %s' % output_filename)
